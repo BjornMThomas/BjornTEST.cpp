ff9f16baf30aa5c7323b3930b094a045a406412e
Bjørn Thomas <159652469+BjornMThomas@users.noreply.github.com>
2024-03-30 22:39:32 -1000

    Add files via upload

---
+++ Robot_splash()elseif.cpp
@@ -0,0 +1,77 @@
+  void superImportantRobotFunctionDONOTREMOVE() {
+    #define RAND_MAX 16
+    int randMessage = rand();
+    if (randMessage == 0) {
+      std::cout << "I am a rat I am a theif I will still your ethernet cable. - The Rat"
+                << std::endl;
+    } else if (randMessage == 1) {
+      std::cout << "erROAR"
+                << std::endl;
+    } else if (randMessage == 2) {
+      std::cout << "Thanks C418!"
+                << std::endl;
+    } else if (randMessage == 3) {
+      std::cout << "we love std::"
+                << std::endl;
+    } else if (randMessage == 4) {
+      std::cout << "Lets try this, hahahahaha..."
+                << std::endl;
+    } else if (randMessage == 5) {
+      std::cout << "|:~{"
+                << std::endl;
+    } else if (randMessage == 6) {
+      std::cout << "People told me, you've changed you've changed and I pooped my pants"
+                << std::endl;
+    } else if (randMessage == 7) {
+      std::cout << "I touched the intake and it feel apart. I dont think the robot is ready yet"
+                << std::endl;
+    } else if (randMessage == 8) {
+      std::cout << "Finally fixed ... only took us 7 hours"
+                << std::endl;
+    } else if (randMessage == 9) {
+      std::cout << "Minecraft Music <3"
+                << std::endl;
+    } else if (randMessage == 10) {
+      std::cout << "Roboctopi <3"
+                << std::endl;
+    } else if (randMessage == 11) {
+      std::cout << "BURT"
+                << std::endl;
+    } else if (randMessage == 12) {
+      std::cout << "Team Fourty-Nine Eighteen"
+                << std::endl;
+    } else if (randMessage == 13) {
+      std::cout << "Thank you Jeff! <3"
+                << std::endl;
+    } else if (randMessage == 14) {
+      std::cout << "HANK THE TANK"
+                << std::endl;
+    } else if (randMessage == 15) {
+      std::cout << "go check out stantoncomet.github.io!"
+                << std::endl;
+    } else if (randMessage == 16) {
+      std::cout << "Time for a snack break"
+                << std::endl;
+    } else if (randMessage == 17) {
+      std::cout << "Crabs are like jelly donuts"
+                << std::endl;
+    } else if (randMessage == 18) {
+      std::cout << "We're not magic ... just coders"
+                << std::endl;
+    } else if (randMessage == 19) {
+      std::cout << "There's a hole in the bottom of my brain"
+                << std::endl;
+    } else if (randMessage == 20) {
+      std::cout << "Thank you for your paitience"
+                << std::endl;
+    } else if (randMessage == 21) {
+      std::cout << "There's a million things i haven't done but just you wait (Till next year) ;)"
+                << std::endl;
+    } else if (randMessage == 22) {
+      std::cout << "This is basically like a 9-5 job"
+                << std::endl;
+    } else if (randMessage == 22) {
+      std::cout << "Congrats Drive Team We Believe In You!"
+                << std::endl;
+    } 
+  }
